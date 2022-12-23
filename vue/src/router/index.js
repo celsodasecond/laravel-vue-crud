ebe24099dd639055d16be997e39ef7911fbab3b1
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2022-12-23 12:10:22 +0800

    Template-Refactor
    Refactored Tailwind's UI Templates to fit to my project's needs.

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,29 +1,72 @@
 import { createRouter, createWebHistory } from "vue-router";
+// VIEWS
 import Dashboard from '../views/Dashboard.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import Reviews from '../views/Reviews.vue'
+// COMPONENTS
+import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
+// STORE
+import Store from '../store'
 
 const routes = [
     {
         path: '/',
+        redirect: '/dashboard',
         name: 'Dashboard',
-        component: Dashboard
+        component: DefaultLayout,
+        meta: { requiresAuth: true },
+        children: [
+            {
+                path: '/dashboard',
+                name: 'Dashboard',
+                component: Dashboard
+            },
+            {
+                path: '/reviews',
+                name: 'Reviews',
+                component: Reviews
+            },
+        ]
     },
     {
-        path: '/login',
-        name: 'Login',
-        component: Login
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true},
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
+        ]
     },
-    {
-        path: '/register',
-        name: 'Register',
-        component: Register
-    },
+
 ];
 
 const router = createRouter({
     history: createWebHistory(),
     routes
 })
+
+// router.beforeEach((to, from, next) => {
+//     if (to.meta.requiresAuth && !Store.state.user.tokens){
+//         next({name: 'Login'})
+//     } 
+//     else if (Store.state.user.token && (to.meta.isGuest)) {
+//         next({name: 'Dashboard'});
+//     } 
+//     else {
+//         next();
+//     }
+// })
 
 export default router;
